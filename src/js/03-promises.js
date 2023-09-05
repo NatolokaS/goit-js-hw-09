a37300ad528bc37b56b1219a985d9085e6683408
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-09-06 01:28:52 +0300

    task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const { elements: { delay, step, amount } } = event.currentTarget;
+    for (let i = 0; i < amount.value; i++) {
+      let position = i + 1;
+      const delays = Number(delay.value) + step.value*i;
+      createPromise(position, delays).then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  event.currentTarget.reset();
+}
+
+formEl.addEventListener('submit', onSubmitForm);
